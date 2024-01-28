07c6107eb4154e1ed8b36e2ebc0942e577f87c87
DanielC12321 <danielchen25802+1@gmail.com>
2024-01-28 02:42:52 -0600

    sus

--- work.js
+++ work.js
@@ -1,4 +1,25 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
+    const response = axios.get('https://api.search.brave.com/res/v1/web/search', {
+  params: {
+    'q': 'Shawn Chin Texas A&M Linked In'
+  },
+  headers: {
+    'Accept': 'application/json',
+    'Accept-Encoding': 'gzip',
+    'X-Subscription-Token': 'BSAlCBDXr5PwulB9XwmMvBR4pB9KDDP'
+  }
+}).then(response => {
+  // Handle response
+  
+  for(let i = 0; i < response.data.web.results.length; i++) {
+    let obj = response.data.web.results[i];
+
+    console.log(obj.description);
+}
+  //(response.data.web).forEach(ind=>console.log(ind.url));
+}).catch((error) => console.error(error));
     console.log("Script is running")
     document.getElementById('myForm').addEventListener('submit', function(event) {
         event.preventDefault(); // Prevents the default form submission action
@@ -6,6 +27,6 @@ document.addEventListener('DOMContentLoaded', function() {
         // Form data processing
         var name = document.getElementById('search').value;
         alert(name);
-  
+        
     });
 });
