825834df5cf496cc70f7e96702ac2844dd66ec7c
xxilytoo <contactkenhao@gmail.com>
2024-01-27 21:55:06 -0600

    search display

---
+++ searchdisplay.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    alert("Script is running");
+    const urls = ["https://www.merriam-webster.com/dictionary/test","https://www.dictionary.com/browse/test"];
+    const description = ["Medical Definition · 1 of 3 noun. ˈtest. 1. : a critical examination, observation, evaluation, or trial · 2 of 3 transitive verb. : to subject to a test.", "a particular process or method for trying or assessing. a set of questions, problems, or the like, used as a means of evaluating the abilities, aptitudes, ..."]
+    const title = ["Test Definition & Meaning", "TEST Definition & Usage Examples"]
+    for(let i = 0; i < urls.length; i++){
+        $("")
+    }
+});
