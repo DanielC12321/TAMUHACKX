f289ab90d78d0469a974e43f522beb21941594eb
xxilytoo <contactkenhao@gmail.com>
2024-01-28 03:03:19 -0600

    take away alert testing in beg

--- searchdisplay.js
+++ searchdisplay.js
@@ -1,4 +1,3 @@
-alert("Script is running");
 const urls = ["https://www.merriam-webster.com/dictionary/test","https://www.dictionary.com/browse/test"];
 const description = ["Medical Definition · 1 of 3 noun. ˈtest. 1. : a critical examination, observation, evaluation, or trial · 2 of 3 transitive verb. : to subject to a test.", "a particular process or method for trying or assessing. a set of questions, problems, or the like, used as a means of evaluating the abilities, aptitudes, ..."]
 const title = ["Test Definition & Meaning", "TEST Definition & Usage Examples"]
